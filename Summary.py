5f0924e116cd7a414142df0da62df23ee8400df8
Ambika <ambika199820@gmail.com>
2023-09-06 16:24:41 +0530

    Add files via upload

---
+++ Summary.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import newspaper
+
+
+st.title('Article scraper')
+url = st.text_input('', placeholder='Paste URL and Enter')
+
+if url:
+    article = newspaper.Article(url)
+    article.download()
+
+    article.parse()
+    authors = article.authors
+    st.text(','.join(authors))
+
+   
+   
+    article.nlp()
+    st.subheader('Keywords')
+    key = article.keywords
+    st.write(', '.join(key))
+
+
+
+
+
+    tab1, tab2 = st.tabs(['Full Text', 'Summary'])
+    with tab1:
+        article.text
+    with tab2:
+        st.write(article.summary)    
+
